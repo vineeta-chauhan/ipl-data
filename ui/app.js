c6903b4c0665771fa09b7d84d87a6d174b6d2f1a
vineeta-chauhan <vineetachauhan69@gmail.com>
2018-12-23 11:27:48 +0530

    ui initialise

---
+++ ui/app.js
@@ -0,0 +1,11 @@
+const express = require('express' )
+const app = express()
+const port = 3000
+
+console.log(__dirname);
+app.get('/msg',(req, res) => res.send('Hello World in msg root!')) 
+app.get('/index', (req, res) => res.send('Hello World!')  )
+app.get('/', function(req, res){
+    res.sendFile('index.html',{root: __dirname});
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
